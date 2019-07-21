2ecc9b8e18e043518f5ae37b0208d00d488335bc
ybonest <ybogitchi@yeah.net>
2019-07-21 22:31:35 +0800

    feat: build

--- scripts/rollup/build.js
+++ scripts/rollup/build.js
@@ -3,25 +3,57 @@ const path = require('path');
 const Bundle = require('./bundles');
 const common = require('./common');
 
+function outputBundleNames(bundleType, entry) {
+    let basePath = 'build/dist'
+    switch(bundleType) {
+        case 'UMD_DEV':
+            return `${basePath}/${entry}/umd/${entry}.development.js`;
+        case 'UMD_PROD':
+            return `${basePath}/${entry}/umd/${entry}.production.js`;
+        case 'NODE_DEV':
+            return `${basePath}/${entry}/cjs/${entry}.development.js`;
+        case 'NODE_PROD':
+            return `${basePath}/${entry}/cjs/${entry}.production.js`;
+    }
+}
+
+function outputFormat(bundleType) {
+    switch(bundleType) {
+        case 'UMD_DEV':
+        case 'UMD_PROD':
+            return 'umd';
+        case 'NODE_DEV':
+        case 'NODE_PROD':
+            return 'cjs';
+    }
+}
+
+
+
 async function buildBundles(options) {
-    const inputFiles = path.join(__dirname, '../../packages', options.entry, 'index.ts');
-    
+    const { types, entry, external } = options;
+    const test = require.resolve(entry);
+
+    console.log(test, "(((")
+    const inputFiles = path.join(__dirname, '../../packages', entry, 'index.ts');
     const inputOptions = {
         input: inputFiles,
-        external: options.external,
+        external: external,
         plugins: common.plugins(options),
     };
-    const outputOptions = {
-        file: `build/dist/${options.entry}.js`,
-        format: 'amd'
-    }
-    const bundle = await rollup.rollup(inputOptions);
 
-    await bundle.write(outputOptions);
+    for(let type of types) {
+        const outputOptions = {
+            file: outputBundleNames(type, entry),
+            format: outputFormat(type)
+        }
+        const bundle = await rollup.rollup(inputOptions);
+    
+        await bundle.write(outputOptions);
+    }
 }
 
 async function buildEverything() {
-    
     for(let item of Bundle.bundles) {
         await buildBundles(item);
     }
--- scripts/rollup/bundles.js
+++ scripts/rollup/bundles.js
@@ -1,5 +1,13 @@
+const typesMap = {
+    UMD_DEV: 'UMD_DEV',
+    UMD_PROD: 'UMD_PROD',
+    NODE_DEV: 'NODE_DEV',
+    NODE_PROD: 'NODE_PROD'
+}
+
 const bundles = [
     {
+        types: [ typesMap.UMD_DEV, typesMap.UMD_PROD, typesMap.NODE_DEV, typesMap.NODE_PROD ],
         entry: 'element',
         external: []
     }
