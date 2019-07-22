8f5cad97cbb4cd0046b7fe1152e49f10043e990b
yanxiaobo <yanxiaobo@chengfayun.com>
2019-07-22 19:04:10 +0800

    feat: build

--- element.js
+++
@@ -1,7 +0,0 @@
-define(function () { 'use strict';
-
-	const Ind = require('./src/indy');
-
-	module.exports = Ind;
-
-});
--- scripts/rollup/build.js
+++ scripts/rollup/build.js
@@ -1,62 +1,122 @@
 const rollup = require('rollup');
-const path = require('path');
+const ncp = require('ncp');
+const fs = require('fs');
+const rimraf = require('rimraf');
+const chalk = require('chalk')
+
 const Bundle = require('./bundles');
 const common = require('./common');
 
 function outputBundleNames(bundleType, entry) {
-    let basePath = 'build/dist'
-    switch(bundleType) {
-        case 'UMD_DEV':
-            return `${basePath}/${entry}/umd/${entry}.development.js`;
-        case 'UMD_PROD':
-            return `${basePath}/${entry}/umd/${entry}.production.js`;
-        case 'NODE_DEV':
-            return `${basePath}/${entry}/cjs/${entry}.development.js`;
-        case 'NODE_PROD':
-            return `${basePath}/${entry}/cjs/${entry}.production.js`;
-    }
+  let basePath = 'build/dist';
+  switch (bundleType) {
+    case 'UMD_DEV':
+      return `${basePath}/${entry}/umd/${entry}.development.js`;
+    case 'UMD_PROD':
+      return `${basePath}/${entry}/umd/${entry}.production.js`;
+    case 'NODE_DEV':
+      return `${basePath}/${entry}/cjs/${entry}.development.js`;
+    case 'NODE_PROD':
+      return `${basePath}/${entry}/cjs/${entry}.production.js`;
+  }
 }
 
 function outputFormat(bundleType) {
-    switch(bundleType) {
-        case 'UMD_DEV':
-        case 'UMD_PROD':
-            return 'umd';
-        case 'NODE_DEV':
-        case 'NODE_PROD':
-            return 'cjs';
-    }
+  switch (bundleType) {
+    case 'UMD_DEV':
+    case 'UMD_PROD':
+      return 'umd';
+    case 'NODE_DEV':
+    case 'NODE_PROD':
+      return 'cjs';
+  }
 }
 
+function transformHOC(replace) {
+  return (read, write) => {
+    if (replace) {
+      read.setEncoding('utf8');
+      read.on('data', chunk => {
+        chunk = replace(chunk) || chunk;
+        write.write(chunk);
+        write.end();
+      });
+    } else {
+      read.pipe(write);
+    }
+  };
+}
 
+function copyAsync(source, destination, replace) {
+  const transform = transformHOC(replace);
+
+  return new Promise((resolve, reject) => {
+    ncp(source, destination, { transform }, function(err) {
+      if (err) {
+        reject();
+        return console.error(err);
+      }
+      resolve();
+    });
+  });
+}
+
+function replacePackageMain(chunk) {
+  if (typeof chunk === 'string') {
+    return chunk.replace('index.ts', 'index.js');
+  }
+}
+
+function deleteBuildPackage(path, callback) {
+  rimraf(path, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      callback && callback();
+    }
+  })
+}
+
+// copy 其余文件到对应dist中
+async function copyRelatedFileToDist(name) {
+  await copyAsync(
+    `packages/${name}/package.json`,
+    `build/dist/${name}/package.json`,
+    replacePackageMain
+  );
+  await copyAsync(`packages/${name}/README.md`, `build/dist/${name}/README.md`);
+  await copyAsync(`packages/${name}/npm`, `build/dist/${name}`);
+}
+
+function initNpmPackage() {
+  return Promise.resolve(fs.readdirSync('packages').forEach(copyRelatedFileToDist));
+}
 
 async function buildBundles(options) {
-    const { types, entry, external } = options;
-    const test = require.resolve(entry);
+  const { types, entry, external } = options;
+  const inputEntry = require.resolve(entry); // 与yarn的workspaces配合使用，否则默认加载的是node_modules中的包
 
-    console.log(test, "(((")
-    const inputFiles = path.join(__dirname, '../../packages', entry, 'index.ts');
-    const inputOptions = {
-        input: inputFiles,
-        external: external,
-        plugins: common.plugins(options),
+  const inputOptions = {
+    input: inputEntry,
+    external: external,
+    plugins: common.plugins(options)
+  };
+
+  for (let type of types) {
+    const outputOptions = {
+      file: outputBundleNames(type, entry),
+      format: outputFormat(type)
     };
-
-    for(let type of types) {
-        const outputOptions = {
-            file: outputBundleNames(type, entry),
-            format: outputFormat(type)
-        }
-        const bundle = await rollup.rollup(inputOptions);
-    
-        await bundle.write(outputOptions);
-    }
+    const bundle = await rollup.rollup(inputOptions);
+    await bundle.write(outputOptions);
+  }
 }
 
 async function buildEverything() {
-    for(let item of Bundle.bundles) {
-        await buildBundles(item);
-    }
+  for (let item of Bundle.bundles) {
+    await buildBundles(item);
+  }
+  await initNpmPackage();
 }
 
-buildEverything()
+deleteBuildPackage('build', buildEverything)
