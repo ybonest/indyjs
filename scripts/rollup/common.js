e10595cfcca539ca8167be8c91741ea2e3366d6e
yanxiaobo <yanxiaobo@chengfayun.com>
2019-07-22 19:04:10 +0800

    feat: build

--- scripts/rollup/common.js
+++ scripts/rollup/common.js
@@ -2,10 +2,12 @@ const babel = require('rollup-plugin-babel');
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 
+
+
 function plugins(options) {
-    return [ resolve(), commonjs(), babel() ]
+  return [resolve({ external: options.external }), commonjs(), babel()];
 }
 
 module.exports = {
-    plugins
-}
+  plugins
+};
